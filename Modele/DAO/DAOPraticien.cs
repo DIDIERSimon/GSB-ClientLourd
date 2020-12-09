60a92598b76e4b6131c3140e9141d6fcf8613f49
Simon Didier <simon.didier200@gmail.com>
2020-12-09 09:52:10 +0100

    Modèles de l'application

---
+++ Modele/DAO/DAOPraticien.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using PPE3.Metier;
+using Persistance;
+
+namespace DAO
+{
+	public class DAOPraticien
+	{
+
+		///<summary>
+		///Fournit la liste de tous les rapportvisites.
+		///</summary>
+		///<returns>
+		///Une collection (list) de rapportvisites
+		///</returns>
+		public static List<Praticien> GetPraticien()
+		{
+			//Collection de rapportvisite qui sera retournée à la couche présentation.
+			//Cette dernière ne doit travailler qu'avec des objets métiers.
+			List<Praticien> praticien = null;
+			//La requête à exécuter pour sélectionner tous les rapportvisites
+			string selectPraticien = "SELECT PRA_NUM, PRA_NOM, PRA_PRENOM, PRA_ADRESSE, PRA_CP, PRA_VILLE, PRA_COEFNOTORIETE FROM praticien";
+			//On appelle la couche persistance pour faire exécuter la requête. On exécute la méthode statique Select de la classe DBInterface
+			//Rappel : la méthode Select retourne un datatable
+			DataTable dataTable = DBInterface.Select(selectPraticien);
+			//Le Datatable retourné est transformé en collection de rapportvisite
+			if (dataTable != null)
+			{
+				praticien = new List<Praticien>();
+				foreach (DataRow row in dataTable.Rows)
+				{
+					Praticien praticiens = CreerPraticien(row);
+					praticien.Add(praticiens);
+				}
+			}
+			return praticien;
+		}
+
+		//Construction d'une instance de rapportvisite à partir d'une ligne du résultat de la requête.
+		//Rappel : on souhaite que la couche "Présentation" ne soit pas liée à la base de données.
+		//Elle ne doit travailler qu'avec des objets métiers.
+		private static Praticien CreerPraticien(DataRow row)
+		{
+			Praticien praticien= new Praticien();
+			praticien.NumPraticien = row["PRA_NUM"].ToString();
+			praticien.NomPraticien = row["PRA_NOM"].ToString();
+			praticien.PrenomPraticien = row["PRA_PRENOM"].ToString();
+			praticien.AdressePraticien = row["PRA_ADRESSE"].ToString();
+			praticien.CpPraticien = row["PRA_CP"].ToString();
+			praticien.VillePraticien = row["PRA_VILLE"].ToString();
+			praticien.CoefNotorietePraticien = row["PRA_COEFNOTORIETE"].ToString();
+			return praticien;
+		}
+
+		public static Praticien GetPraticienNomPrenom(string nom, string prenom)
+		{
+			Praticien praticien = null;
+			string selectPraticien = "SELECT PRA_NUM, PRA_NOM, PRA_PRENOM, PRA_ADRESSE, PRA_CP, PRA_VILLE, PRA_COEFNOTORIETE, TYP_CODE FROM PRATICIEN WHERE PRA_NOM='" + nom + "' AND PRA_PRENOM='" + prenom + "'";
+			DataTable dataTable = DBInterface.Select(selectPraticien);
+			if (dataTable.Rows.Count != 0)
+			{
+				DataRow row = dataTable.Rows[0];
+				praticien = CreerPraticien(row);
+			}
+
+			return praticien;
+		}
+	}
+}
---
+++ Modele/DAO/DAORapportVisite.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using PPE3.Metier;
+using Persistance;
+
+namespace DAO
+{
+	public class DAORapportVisite
+	{
+
+		///<summary>
+		///Fournit la liste de tous les rapportvisites.
+		///</summary>
+		///<returns>
+		///Une collection (list) de rapportvisites
+		///</returns>
+		public static List<RapportVisite> GetRapportVisite()
+		{
+			//Collection de rapportvisite qui sera retournée à la couche présentation.
+			//Cette dernière ne doit travailler qu'avec des objets métiers.
+			List<RapportVisite> rapportvisite = null;
+			//La requête à exécuter pour sélectionner tous les rapportvisites
+			string selectRapportVisite = "SELECT RAP_NUM, RAP_DATE, RAP_BILAN, RAP_MOTIF, PRA_NUM, VIS_MATRICULE FROM rapportvisite";
+			//On appelle la couche persistance pour faire exécuter la requête. On exécute la méthode statique Select de la classe DBInterface
+			//Rappel : la méthode Select retourne un datatable
+			DataTable dataTable = DBInterface.Select(selectRapportVisite);
+			//Le Datatable retourné est transformé en collection de rapportvisite
+			if (dataTable != null)
+			{
+				rapportvisite = new List<RapportVisite>();
+				foreach (DataRow row in dataTable.Rows)
+				{
+					RapportVisite rapportsvisites = CreerRapportVisite(row);
+					rapportvisite.Add(rapportsvisites);
+				}
+			}
+			return rapportvisite;
+		}
+
+		//Construction d'une instance de rapportvisite à partir d'une ligne du résultat de la requête.
+		//Rappel : on souhaite que la couche "Présentation" ne soit pas liée à la base de données.
+		//Elle ne doit travailler qu'avec des objets métiers.
+		private static RapportVisite CreerRapportVisite(DataRow row)
+		{
+			RapportVisite rapportvisite = new RapportVisite();
+			rapportvisite.NumRapport = row["RAP_NUM"].ToString();
+			rapportvisite.DateRapport = row["RAP_DATE"].ToString();
+			rapportvisite.BilanRapport = row["RAP_BILAN"].ToString();
+			rapportvisite.MotifRapport = row["RAP_MOTIF"].ToString();
+			rapportvisite.Pra_numRapport = int.Parse(row["PRA_NUM"].ToString());
+			rapportvisite.Vis_matriculeRapport = row["VIS_MATRICULE"].ToString();
+			return rapportvisite;
+		}
+
+		public static Boolean CreateRapport(RapportVisite rapport)
+		{
+			RapportVisite nouveauRapport = null;
+			string createRapport = "INSERT INTO RAPPORTVISITE VALUES ('" + rapport.NumRapport + "', '" + rapport.DateRapport + "', '" + rapport.BilanRapport + "', '" + rapport.MotifRapport + "', '" + rapport.Pra_numRapport + "', '" + rapport.Vis_matriculeRapport + "');";
+			int updatedRows = DBInterface.Update(createRapport);
+			if (updatedRows > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public static Boolean UpdateRapport(RapportVisite rapport)
+		{
+			string updateRapport = "UPDATE RAPPORTVISITE SET RAP_DATE='" + rapport.DateRapport + "', RAP_BILAN='" + rapport.BilanRapport + "', RAP_MOTIF='" + rapport.MotifRapport + "', PRA_NUM='" + rapport.Pra_numRapport.ToString() + "' WHERE RAP_NUM='" + rapport.NumRapport + "';";
+			int updatedRows = DBInterface.Update(updateRapport);
+			if (updatedRows > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
